97e65bce4515ece9dfd97e26afdf32df78656c4d
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-07-17 20:27:57 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import CodeMirror from "react-codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/javascript/javascript";
+import "./App.css";
+import domtoimage from "dom-to-image";
 
 function App() {
+  const [screenshot, setScreenshot] = useState(null);
+  const codeRef = useRef(null);
+
+  const takePic = () => {
+    domtoimage
+      .toPng(codeRef.current)
+      .then(dataUrl => setScreenshot(dataUrl))
+      .catch(error => {
+        console.error("oops, something went wrong!", error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="column">
+        <div ref={codeRef}>
+          <CodeMirror
+            options={{
+              mode: "javascript",
+              lineNumbers: true,
+              theme: "material",
+              viewportMargin: Infinity
+            }}
+            onChange={() => {
+              setScreenshot(null);
+            }}
+          />
+        </div>
+      </div>
+      <div className="column">
+        {screenshot ? (
+          <img src={screenshot} alt="Code screenshot" />
+        ) : (
+          <button onClick={() => takePic()}>Generate Screenshot</button>
+        )}
+      </div>
     </div>
   );
 }
-
 export default App;
