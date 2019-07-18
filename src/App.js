c90b58d402c46c810d05db3146f0843c8d49077b
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-07-17 21:32:30 -0400

    Add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import "codemirror/theme/material.css";
 import "codemirror/mode/javascript/javascript";
 import "./App.css";
 import domtoimage from "dom-to-image";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, Button, Row, Col } from "react-bootstrap";
+
+const initialCode = `// Enter code here...
+
+`;
 
 function App() {
   const [screenshot, setScreenshot] = useState(null);
@@ -20,30 +26,50 @@ function App() {
   };
 
   return (
-    <div className="row">
-      <div className="column">
-        <div ref={codeRef}>
-          <CodeMirror
-            options={{
-              mode: "javascript",
-              lineNumbers: true,
-              theme: "material",
-              viewportMargin: Infinity
-            }}
-            onChange={() => {
-              setScreenshot(null);
-            }}
-          />
-        </div>
+    <React.Fragment>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand>Code Screenshots</Navbar.Brand>
+      </Navbar>
+      <div class="container-fluid">
+        <Row>
+          <Col className="column">
+            <div ref={codeRef}>
+              <CodeMirror
+                value={initialCode}
+                options={{
+                  mode: "javascript",
+                  lineNumbers: true,
+                  theme: "material",
+                  viewportMargin: Infinity
+                }}
+                onChange={() => {
+                  setScreenshot(null);
+                }}
+              />
+            </div>
+          </Col>
+          <Col className="column">
+            {screenshot ? (
+              <img src={screenshot} alt="Code screenshot" />
+            ) : (
+              <div className="preview-area">
+                Your screenshot will appear here...
+              </div>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center column">
+            <Button onClick={() => takePic()}>Generate Screenshot</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center column">
+            Created by <a href="https://www.twitter.com/nas5w">Nick Scialli</a>
+          </Col>
+        </Row>
       </div>
-      <div className="column">
-        {screenshot ? (
-          <img src={screenshot} alt="Code screenshot" />
-        ) : (
-          <button onClick={() => takePic()}>Generate Screenshot</button>
-        )}
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 export default App;
